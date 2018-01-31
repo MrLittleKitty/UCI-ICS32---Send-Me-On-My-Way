739becf7c531894c80554238e22721d28d9d2f87
Eric Wolfe <nuclearcat1337@gmail.com>
2018-01-30 16:39:43 -0800

    Working console game and updated library methods

--- connectfour_console.py
+++ connectfour_console.py
@@ -7,6 +7,29 @@ def start_game() -> None:
     The main entry point for the program.
     Starts the game of Connect Four.
     """
+    gameState = connect.new_game()
+    while connect.winner(gameState) == connect.NONE:
+        lib.print_game_state(gameState)
+        print('')
+        while (True):
+            col, move = lib.prompt_and_get_move(gameState)
+            try:
+                newgameState = None
+                if move == lib.DROP_TOP:
+                    newgameState = connect.drop(gameState, col)
+                else:
+                    newgameState = connect.pop(gameState, col)
+                gameState = newgameState
+                break
+            except connect.InvalidMoveError:
+                if move == lib.POP_BOTTOM:
+                    print('Can not pop on the given column')
+                else:
+                    print('Can not drop on the given column')
+    winner = connect.winner(gameState)
+    print()
+    lib.print_game_state(gameState)
+    print(lib.get_player_string(winner) + ' has won the game! Congrats!')
     return
 
 
--- connectfour_library.py
+++ connectfour_library.py
@@ -1,5 +1,36 @@
 import connectfour as lib
 
+DROP_TOP = -2
+POP_BOTTOM = -3
+
+
+def get_player_string(player: int) -> str:
+    """
+    Returns a string representation of the given player number
+    :param player: An integer value representing a player
+    :return: A string that represents the given player
+    """
+    if player == lib.YELLOW:
+        return 'Yellow'
+    elif player == lib.RED:
+        return 'Red'
+    else:
+        return 'None'
+
+
+def get_player_symbol(player: int) -> str:
+    """
+    Returns a single character representation of the given player number
+    :param player: An integer value representing a player
+    :return: A single character that represents the given player
+    """
+    if player == lib.YELLOW:
+        return 'Y'
+    elif player == lib.RED:
+        return 'R'
+    else:
+        return '.'
+
 
 def print_game_state(state: lib.GameState) -> None:
     """
@@ -9,7 +40,35 @@ def print_game_state(state: lib.GameState) -> None:
     for rowIdx in range(lib.BOARD_ROWS):
         for colIdx in range(lib.BOARD_COLUMNS):
             if colIdx == 0:
-                print(str(state.board[colIdx][rowIdx]), end='')
+                print(get_player_symbol(state.board[colIdx][rowIdx]), end='')
             else:
-                print(' ' + str(state.board[colIdx][rowIdx]), end='')
+                print(' ' + get_player_symbol(state.board[colIdx][rowIdx]), end='')
         print('')
+
+
+def prompt_and_get_move(state: lib.GameState) -> (int, str):
+    """
+    Prompts the current player to enter a valid column number and a valid move type
+    :param state: The current state of the game
+    :return: A tuple whose first value is the entered column number and whose second value is the move type
+    """
+    print('It is the ' + get_player_string(state.turn) + ' players turn')
+    while (True):
+        print('Please input a column number (1-7) and a move (drop,pop). Example: 3 drop')
+        line = input().strip()
+        colString = line[0:1]
+        move = line[2:]
+
+        if not colString.isdigit():
+            continue
+
+        colNumber = int(colString)
+        if colNumber < 1 or colNumber > 7:
+            continue
+
+        move = move.strip().lower()
+        if move == 'drop':
+            return colNumber - 1, DROP_TOP
+
+        if move == 'pop':
+            return colNumber - 1, POP_BOTTOM
